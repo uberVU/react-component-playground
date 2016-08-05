d319f8384b42550efb1f9f96e39cc4b4cf06823a
Bogdan Stefan <bogdan.stefan@hootsuite.com>
2016-08-05 17:07:52 +0300

    Delete unused eslint globals

--- tests/.eslintrc.js
+++ tests/.eslintrc.js
@@ -3,25 +3,16 @@ module.exports = {
 
   'globals': {
     /* testing objects */
-    'casper': false,
-    'flow': false,
-    'phantomCSS': false,
     'document': false,
-    'sandbox': false,
-    'TestHelpers': false,
     'sinon': false,
     'window': false,
-    '_': false,
 
     /* testing methods */
-    'before': false,
     'beforeEach': false,
-    'after': false,
     'afterEach': false,
     'describe': false,
     'expect': false,
     'it': false,
-    'step': false
   },
 
   'rules': {
